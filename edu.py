0cfdabf40ebe3210212551d88f2d9e2c40fb1c36
Tim Albersmann <n.ah.endres@gmail.com>
2021-03-17 08:46:30 +0100

    ignoring private files

--- edu.py
+++ edu.py
@@ -1,12 +1,47 @@
-import userdata
-from edupage_api import Edupage
-
-#my_username = userdata.edupage_tim.username
-#my_password = userdata.edupage_tim.password
-
-my_username = "xtialbe5"
-my_password = "pzyyj1"
+import data, connection
+import json
+from edupage_api import *
+from datetime import datetime
 
 
-edupage = Edupage(my_username, my_password)
-print(edupage.login())
+internet_connection = connection.connect()
+print("connected" if internet_connection else "no internet!")
+if not internet_connection: raise ConnectionError("no internet connection")
+
+with open("tts.json", "r") as f:
+    tts = json.load(f)
+
+my_group = "group 1"
+my_username = data.edupage_tim.username()
+my_password = data.edupage_tim.password()
+subdomain = data.edupage.subdomain()
+
+edupage = Edupage(subdomain, my_username, my_password)
+
+try:
+    edupage.login()
+except BadCredentialsException:
+    print("Wrong username or password!")
+except LoginDataParsingException:
+    print("Try again or open an issue!")
+if not edupage.is_logged_in:
+    raise ConnectionError("couldn't connect to edupage")
+
+print("is logged in: ", edupage.is_logged_in)
+if edupage.is_logged_in:
+    print("my user id:", edupage.get_user_id())
+
+    dates = edupage.get_available_timetable_dates()
+    timetable = edupage.get_timetable(dates)
+    if timetable == None: pass
+    else: print("timetable: ", timetable)
+if data.system() == "Windows":
+    now = datetime.datetime.now()
+elif data.system() == "Linux":
+    now = datetime.now()
+
+
+while not timetable: 
+    for meeting_time in tts ["starttimes"][my_group]:
+        if meeting_time in now.strftime("%H:%M"):
+            print("wake up, it's time for school man, c'mon")
---
+++ link_checker_all_links_autoassign.py
@@ -0,0 +1,65 @@
+import os
+import time
+import json
+
+from selenium.webdriver.remote.errorhandler import ErrorCode
+import data
+import platform
+from datetime import datetime
+from selenium import webdriver
+
+def dicttemplate(tn, cn, l): 
+    return dict(
+        {
+                "name": tn,
+                "call_name": cn,
+                "link": l
+        }    
+)
+#get login id
+my_username = data.intranet_tim.username()
+my_password = data.intranet_tim.password()
+#open teachers json
+with open("tts.json", "r") as teacherf:
+    file = json.load(teacherf)
+#open browser depending on operating system
+if platform.system() == "Linux":
+    browser = webdriver.Firefox()
+elif platform.system() == "Windows":
+    browser = webdriver.Chrome("C:\\.programming\\drivers_extra_data\\chromedriver.exe")
+else: raise NameError("what sys r u running on mate? try installing firefox!")
+browser.get(data.school.url())
+time.sleep(3)
+#find id and password
+id_box = browser.find_element_by_name('u')
+pass_box = browser.find_element_by_name('p')
+#send login info
+id_box.send_keys(my_username)
+pass_box.send_keys(my_password)
+#click login
+login_button = browser.find_element_by_xpath("//*[@id='dw__login']/div/fieldset/button")
+login_button.click()
+#finding the url's
+teacher_element_list = browser.find_elements_by_class_name("urlextern")
+#deleting the teacher entrys
+for l in range(len(file["teachers"])):
+    file["teachers"].pop(0)
+#formatting the teachers names and writing them in the dict
+for element in teacher_element_list:
+    if "," in element.text:
+        print(element.text)
+        t = element.text
+        comma = t.find(",")
+        fl = t[comma + 2]
+        n = t[:comma]
+        
+        nice_name = fl + ". " + n
+        call_name = t
+        link = element.get_attribute('href')
+        
+        teacherd = dicttemplate(nice_name, call_name, link)
+        file["teachers"].append(teacherd)
+# saving the json file
+with open ('all_teachers_new.json', 'w') as teacherf:
+    json.dump(file, teacherf, indent=4)
+browser.quit()
