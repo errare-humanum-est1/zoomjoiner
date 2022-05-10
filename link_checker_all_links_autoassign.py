d9fb8326d2fb7e7285519796b68d2f09d03c57a4
Tim <n.ah.endres@gmail.com>
2022-05-10 17:58:44 +0200

    complete refactor and makeover

--- link_checker_all_links_autoassign.py
+++ link_checker_all_links_autoassign.py
@@ -1,61 +1,77 @@
 #!/usr/bin/env python
 
 import time, json, platform
-import data
+from selenium import * 
 from selenium import webdriver
 
-def dicttemplate(tn, cn, l): 
+def dicttemplate(teacherName, callName, link): 
     return dict(
         {
-                "name": tn,
-                "call_name": cn,
-                "link": l
+                "name": teacherName,
+                "call_name": callName,
+                "link": link
         }    
 )
-#get login id
-my_username = data.userdata.intranet_username
-my_password = data.userdata.intranet_password
+
+with open("config.json", "r") as configFile:
+    config = json.load(configFile)
+
 #open teachers json
-with open("tts.json", "r") as teacherf:
-    file = json.load(teacherf)
+with open("timetable_teachers.json", "r") as teacherFile:
+    infoDict = json.load(teacherFile)
+
 #open browser depending on operating system
 if platform.system() == "Linux":
     browser = webdriver.Firefox()
 elif platform.system() == "Windows":
     browser = webdriver.Chrome("C:\\.programming\\drivers_extra_data\\chromedriver.exe")
 else: raise NameError("what sys r u running on mate? try installing firefox!")
-browser.get(data.info.school_website_url())
+
+browser.get(config["school_website_url"])
 time.sleep(3)
+
 #find id and password
-id_box = browser.find_element_by_name('u')
-pass_box = browser.find_element_by_name('p')
-#send login info
-id_box.send_keys(my_username)
-pass_box.send_keys(my_password)
+browser.find_element_by_name("u").send_keys(config["intranet_username"])
+browser.find_element_by_name("p").send_keys(config["intranet_password"])
+
 #click login
-login_button = browser.find_element_by_xpath("//*[@id='dw__login']/div/fieldset/button")
-login_button.click()
+browser.find_element_by_xpath("//*[@id='dw__login']/div/fieldset/button").click()
+
 #finding the url's
 teacher_element_list = browser.find_elements_by_class_name("urlextern")
+
 #deleting the teacher entrys
-for l in range(len(file["teachers"])):
-    file["teachers"].pop(0)
+infoDict["teachers"].clear()
+
 #formatting the teachers names and writing them in the dict
 for element in teacher_element_list:
-    if "," in element.text:
+    if "https://zoom.us/j/" in element.get_attribute("href"):
+
         print(element.text)
-        t = element.text
-        comma = t.find(",")
-        fl = t[comma + 2]
-        n = t[:comma]
+        name = element.text.split()
+
+        name.reverse()
         
-        nice_name = fl + ". " + n
-        call_name = t
-        link = element.get_attribute('href')
-        
-        teacherd = dicttemplate(nice_name, call_name, link)
-        file["teachers"].append(teacherd)
+        match len(name):
+            case 1:
+                lastName = shortName = f"{name[0]}"
+            case 2:
+                shortName = f"{name[0][0]}. {name[1][:-1]}"
+                lastName = f"{name[1][:-1]}"
+            case 3:
+                shortName = f"{name[1]} {name[2][:-1]} {name[0]}"
+                lastName = f"{name[2][:-1]}"
+
+        infoDict["teachers"].append(
+            dicttemplate(
+                shortName, 
+                lastName, 
+                element.get_attribute('href')
+                )
+            )
+
 # saving the json file
-with open ('tts.json', 'w') as teacherf:
-    json.dump(file, teacherf, indent=4)
+with open ('timetable_teachers.json', 'w') as teacherFile:
+    json.dump(infoDict, teacherFile, indent=4)
+
 browser.quit()
--- zoom_connector.py
+++ zoom_connector.py
@@ -1,284 +1,157 @@
 #!/usr/bin/env python
 
 import os, platform
-try: 
-    import data
-    import json, time, webbrowser, urllib.request, platform, pyautogui
-    from edupage_api import *
-    from datetime import datetime as dt, timedelta as td
-    from termcolor import colored as cl
-    import logging as lg
-    if platform.system() == "Windows":
-        from pywinauto import Desktop  # type: ignore (this makes pylance stfu and not show me a warning :) )
-except ImportError:  
-    if platform.system() == "Windows": os.system("pip install -r requirements.txt")
-    elif platform.system() == "Linux": os.system("pip3 install -r requirements.txt")
-    try:
-        import data
-        import json, time, webbrowser, urllib.request, platform, pyautogui
-        from edupage_api import *
-        from datetime import datetime as dt, timedelta as td #type:ignore
-        from termcolor import cprint as cl #type:ignore
-        import logging as lg
-        if platform.system() == "Windows":
-            from pywinauto import Desktop  # type: ignore (this makes pylance stfu and not show me a warning :) )
-    except ImportError:
-        print("TRY TO INSTALL THE REQUIREMENTS WITH: pip install -r requirements.txt !!!")
 
-execdir = os.getcwd()
+systemArchitecture = platform.system()
 
-if not os.path.exists("logs"): os.mkdir("logs")
-os.chdir(execdir + "/logs")
+if systemArchitecture == "Windows":
+    from pywinauto import Desktop #type: ignore
 
-counter = 0
-logtime = ""
-unformattet = str(dt.now())
-print("before: ", unformattet)
-for x in unformattet: 
-    if x == " ":
-        x = "_"
-    elif x == ".":
-        break
-    elif x == ":":
-        x = "."
-    logtime += x
-    counter += 1
-print("after: ", logtime)
-logname = "log" + logtime + ".log"
+import json, time, webbrowser, urllib.request, platform, pyautogui, subprocess
+from edupage_api import *
+from datetime import datetime as dt, timedelta as td
+from termcolor import colored as cl
 
-# Create a custom logger
-logger = lg.getLogger(__name__)
-
-# Create handlers
-c_handler = lg.StreamHandler()
-f_handler = lg.FileHandler(logname)
-c_handler.setLevel(lg.DEBUG)
-f_handler.setLevel(lg.DEBUG)
-
-# Create formatters and add it to handlers
-c_format = lg.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = lg.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
-
-# Add handlers to the logger
-logger.addHandler(c_handler)
-logger.addHandler(f_handler)
-
-os.chdir(execdir)
-
-logger.debug("Programm Started.")
 
 #defs
-def check_conn(host='http://google.com'):
+def clearTerminal():
+    os.system("cls" if systemArchitecture == "Windows" else "clear")
+
+def connectToMeeting(link):
     try:
-        urllib.request.urlopen(host)
+        webbrowser.open(link, new = 2, autoraise = False) 
+        print(cl("connecting...", "green"))
+    
+    except Exception as e: 
+        print(cl("there has been an error while connecting", "red"))
+        print(e)
+
+def connection():
+    try:
+        urllib.request.urlopen('http://google.com')
         return True
     except Exception:
-        logger.warning("no internet connection recognized, check your connection or restart the programm")
         return False
 
-def progressBar(current, total, barLength = 20, prefix="Progress: "):
+def progressBar(current, total, barLength = 20, prefix="Progress: ", postfix="sec"):
     percent = float(current) * 100 / total
     arrow   = '-' * int(percent/100 * barLength - 1) + '>'
     spaces  = ' ' * (barLength - len(arrow))
 
-    print(prefix, '[%s%s] %d/ %d sec' % (arrow, spaces, current, total), end='\r')
+    print(f"{prefix}[{arrow}{spaces}] {current}/ {total} {postfix}", end='\r')
 
-def is_hour_between(start, end, fnow):
-    is_between = False
 
-    is_between |= start <= fnow <= end
-    is_between |= end < start and (start <= fnow or fnow <= end)
 
-    return is_between
+clearTerminal()
 
-def get_lesson(wday, lpos):
-    if len(tts["timetable"][wday]) < lpos:
-        return None
-    else: return tts["timetable"][wday][lpos]
 
-def connect(url):
-    try:
-        webbrowser.open(url, new = 2, autoraise = False) 
-        print(cl("connecting...", "green"))
-        logger.debug("connecting to: %s" % teacher)
-    except Exception as e: 
-        print(cl("there has been an error while connecting", "red"))
-        print(e)
-    if not got_shouted_at and breakup_logic:    
-        print("hey sweetie, hope you have fun in ", lesson, " \U0001F618")
+#checks for internet connection, doesnt let go unless its there
+while True:
+    if connection():
+        break
+    time.sleep(30)
 
-internet_connection = check_conn()
-#checks for internet connection
-if not internet_connection: 
-    print("no internet!")
-    raise ConnectionError("no internet connection")
 #loads info json
-with open(execdir + "/tts.json", "r") as f:
-    tts = json.load(f)
+with open(f"{os.getcwd()}/timetable_teachers.json", "r") as f:
+    info = json.load(f)
 
-#access to information script 
-info = data.info()
-userdata = data.userdata
+#access to information 
+with open(f"{os.getcwd()}/config.json", "r") as file:
+    config = json.load(file)
+
 #declaring some variables
 application_start_time = dt.now()
-week_days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+week_days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
-pre_connect = 2
-conn_period = 30
-lesson_lengh = 40
-my_group = "group 1"
-my_username = userdata.edupage_username
-my_password = userdata.edupage_password
-subdomain = userdata.edupage_subdomain
+print("start time: ", dt.now().time())
 
-picture = 'pictures/waiting_for_teacher.png'
-breakup_logic = False
-curr_conn = []
-day_finished = False 
-got_shouted_at = False
 rep = 0
-
-#logs into edupage
-edupage = Edupage(subdomain, my_username, my_password)
-try:
-    edupage.login()
-except BadCredentialsException:
-    print("Wrong username or password!")
-    logger.debug("@edupage wrong credentials")
-except LoginDataParsingException:
-    print("@edupage something is wrong with the edupage api, contact the system administrator")
-if not edupage.is_logged_in:
-    raise ConnectionError("couldn't connect to edupage")
-#checks if logged in in edupage and if there's timetables available
-print("is logged in: ", edupage.is_logged_in)
-if edupage.is_logged_in:
-    print("my user id:", edupage.get_user_id())
-
-    dates = edupage.get_available_timetable_dates()
-    timetable = edupage.get_timetable(dates)
-    if timetable == None: pass
-    else: print("timetable: ", timetable)
-
-now = dt.now()
-print("start time: ", now.time())
-active = True
-while active:
-    #sets time, clears output
+while True:
     now = dt.now()
     in_lesson = False
-    weekend = False
-    if platform.system() == "Windows":
-        os.system("cls")
-    elif platform.system() == "Linux":
-        os.system("clear")
-    
-    print("current time: ", now.time())
-
     #looks up your open windows(linux)
-    if platform.system() == "Linux": 
-        os.chdir(os.getcwd())
-        os.system("wmctrl -l > windows.txt")
-        with open ("windows.txt", "r") as f:
-            windows = f.readlines()
+    if systemArchitecture == "Linux": 
+        proc = subprocess.Popen(["wmctrl -l"], stdout=subprocess.PIPE, shell=True, text=True)
+        (out, err) = proc.communicate()
         
-        for line in windows:
-            if "Zoom Meeting" in line or "Room" in line:
-                in_lesson = True
-                logger.debug("is on meeting")
+        if "Zoom Meeting" in out or "Room" in out:
+            in_lesson = True
 
-        os.system("rm windows.txt")
     #looks up your open windows (windows)
-    elif platform.system() == "Windows": 
+    elif systemArchitecture == "Windows": 
         os.chdir(os.getcwd())
         windows = Desktop(backend="uia").windows()
         for w in windows:
             #checks if you're in a lesson
             if "Zoom Meeting" in w.window_text() or "Breakout" in w.window_text():
                 in_lesson = True
-                logger.debug("is in meeting")
 
-
-    if pyautogui.locateOnScreen(picture) != None:
+    if pyautogui.locateOnScreen('pictures/waiting_for_teacher.png') != None:
         in_lesson = True
-        logger.debug("is in meeting")
 
-    if in_lesson:
-        try: 
-            print("you are currently connected to %s, subject %s" % (curr_conn[0], curr_conn[1]))
-        except IndexError: 
-            if not got_shouted_at and breakup_logic:
-                print(cl("DID YOU CHEAT ON ME?!", "red"))
-                print(cl("YOU'RE CONNECTED BUT DIDN'T CONNECT USING ME?!", "red"))
-                print(cl("I'M BREAKING UP WITH YOU!", "red"))
-                print()
-                got_shouted_at = True
-            elif breakup_logic: print("you are single")
+    if in_lesson: 
+        print("you are currently connected to %s, subject %s" % (currentConnection[0], currentConnection[1]))
 
-    #reads and formats the times
-    times =[]
-    lessonpos = 0
-    for t in tts["starttimes"][my_group]:
-        t = dt.strptime(t, '%H:%M')
-        times.append(t)
-    #checks for lessontime
-    for t in times:
-        tend = t + td(minutes = conn_period)
-        t = t - td(minutes=pre_connect)
-        check_time = is_hour_between(t.time(), tend.time(), now.time())
-        if check_time: break
-        lessonpos += 1
-    
-    #the goddamn friday eception
-    if check_time and week_days[now.weekday()] == "friday": 
-        last = info.return_last_friday()
-        if last == "English":
-            lookup_day = "fridayB"
-        elif last == "French":
-            lookupday = "fridayA"
-        logger.debug("its friday")
-    elif week_days[now.weekday()] == "friday": lookup_day = week_days[int(now.weekday())] + "A"
-    else: lookup_day = week_days[int(now.weekday())]
-    
+    currentConnection = []
+
     #weekend detection
     if week_days[now.weekday()] == "saturday" or week_days[now.weekday()] == "sunday":
         print("it's saturday you idiot" if now.weekday() == 6 else "it's saturday you idiot")
         weekend = True
         break
     
-    #looks up lesson
-    if not weekend and not in_lesson and check_time: 
-        lesson = get_lesson(lookup_day, lessonpos)
-        if lesson == None: 
-            day_finished = True
-            break
-        #gets teacher
-        if not day_finished:    
-            teacher = tts["subjects"][lesson]
-            for tea in tts["teachers"]:
-                if teacher in tea["name"]:
-                    #connects to teacher
-                    curr_conn = [teacher, lesson]
-                    print("you're being connected to %s, and your lesson is %s" % (teacher, lesson))
-                    connect(tea["link"])
+    lessonpos = 0
+    if not in_lesson:
+        #sets time, clears output
+        joinTime = False
+        clearTerminal()
+                
+        print("current time: ", now.time())
         
-            if week_days[now.weekday()] == "friday" and lesson == "English" or lesson == "French":
-                info.set_lastfriday(lesson)
+        #checks if its time to join a lesson
+        for index, lessonStartTime in enumerate(info["starttimes"][config["group"]]):
+            lessonStartTime = dt.strptime(lessonStartTime, '%H:%M')
+
+            if (lessonStartTime - td(minutes=config["pre_connect"])).time() <= now.time() <= (lessonStartTime + td(minutes=config["conn_period"])).time():
+                lookup_day = week_days[int(now.weekday())]
+                lessonpos = index
+                joinTime = True
+                break
+
+        #looks up lesson
+        if joinTime:
+            if len(info["timetable"][lookup_day]) < lessonpos: break
+
+            lesson = info["timetable"][lookup_day][lessonpos]
+
+            #gets teacher
+            connectToTeacher = info["subjects"][lesson]
+            
+            for teacher in info["teachers"]:
+                if not connectToTeacher in teacher["name"]:
+                    continue
+                
+                #connects to teacher
+                currentConnection = [connectToTeacher, lesson]
+                connectToMeeting(teacher["link"])
+                print("you're being connected to %s, and your lesson is %s" % (connectToTeacher, lesson))
+                
+    
     #tells you how long programm has been runnung
-    print()
-    print("this is rep: ", rep)
+    print(f"\nthis is rep: {rep}")
     run_time = str(now - application_start_time)
     run_time_stripped = run_time[:9]
-    print("programm is on since: ", run_time_stripped)
-    print()
+    print(f"programm is on since: {run_time_stripped}\n")
     rep += 1
+
     #that fancy-ass progressbar
     for l in range(60):
         progressBar(l, 60, 50, "Waiting...")
         time.sleep(1)
-    if platform.system() == "Linux": os.system("clear")
-    elif platform.system() == "Windows": os.system("cls")
+    if platform.system() == "Linux": clearTerminal()
+    elif platform.system() == "Windows": clearTerminal()                        
+
+
 #tells you that you're done, if you're done
 print()
 print(cl("good news, you're done for today", "green"))
